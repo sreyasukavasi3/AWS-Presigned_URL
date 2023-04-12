ea4fbfd2c8caf1379d260d84bee6591f01c1ad6c
Sreya <33253396+sreyasukavasi3@users.noreply.github.com>
2023-04-11 23:48:19 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, {Component} from 'react';
+import Uploader from './Components/Uploader';
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        <Uploader />
+      </div>)
+  }
+  
+  
+}
+
+
+export default App;
